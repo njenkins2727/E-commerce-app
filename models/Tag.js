875c490ef8d1549f9756a3a1187a5eb1c4c9e924
njenkins2727 <njenkins2727@gmail.com>
2022-08-10 16:16:10 +0800

    initial push

---
+++ models/Tag.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+const Product = require('./Product.js');
+
+class Tag extends Model {}
+
+Tag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
+    productTag: {
+      type: DataTypes.INTEGER,
+      references: Product.id,//wrong 
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'tag',
+  }
+);
+
+module.exports = Tag;
